ce518e474112bb662527790bfc7b4ad9239ba236
Emily Welsch <emwelsch@gmail.com>
2018-03-13 19:49:21 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,13 +1,14 @@
 require 'pry'
-
+collection = (1..100).to_a
 def my_find(collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
       i += 1
+      binding.pry
   end
 end
 
 # call method with blocks
-# collection = (1..100).to_a
+
 # my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
